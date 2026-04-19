cbb2d81f19aa108c091e86cd8b2e7e339785368c
Cesvic <cesarvictormartinmartin@alumno.ieselrincon.es>
2026-04-19 21:51:37 +0100

    Import added y cambios finales

--- src/services/fb/import-service.js
+++ src/services/fb/import-service.js
@@ -1,4 +1,48 @@
-import { formatToCSV } from "./formatters/csv-formatter";
-import { formatToExcel } from "./formatters/excel-formatter";
-import { formatToJSON } from "./formatters/json-formatter";
-import { formatToXML } from "./formatters/xml-formatter";
+import { addNews } from "./news-service";
+import { parseJSON } from "./parsers/json-parser";
+import { parseCSV } from "./parsers/csv-parser";
+import { parseXML } from "./parsers/xml-parser";
+import { parseExcel } from "./parsers/excel-parser";
+import { deleteAllNews } from "./news-service";
+
+export const importNews = async (file) => {
+  const extension = file.name.split(".").pop().toLowerCase();
+
+  let data;
+
+  switch (extension) {
+    case "json":
+      data = await parseJSON(file);
+      break;
+
+    case "csv":
+      data = await parseCSV(file);
+      break;
+
+    case "xml":
+      data = await parseXML(file);
+      break;
+
+    case "xlsx":
+    case "xls":
+    case "ods":
+      data = await parseExcel(file);
+      break;
+
+    default:
+      alert("Format not supported");
+      return;
+  }
+
+  await deleteAllNews();
+
+  // Guardar en Firebase
+  for (const news of data) {
+    await addNews({
+      title: news.title || "",
+      content: news.content || "",
+      imageUrl: news.imageUrl || null
+    });
+  }
+};
+
--- src/services/fb/news-service.js
+++ src/services/fb/news-service.js
@@ -1,5 +1,5 @@
 import { db } from "./firebase";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc, deleteDoc, onSnapshot } from "firebase/firestore";
 
 export const getNews = async () => {
   const snapshot = await getDocs(collection(db, "news"))
@@ -10,8 +10,29 @@ export const getNews = async () => {
   }));
 };
 
+export const deleteAllNews = async () => {
+  const snapshot = await getDocs(collection(db, "news"));
+
+  const deletions = snapshot.docs.map(doc =>
+    deleteDoc(doc.ref)
+  );
+
+  await Promise.all(deletions);
+};
+
 export const addNews = async (news) => {
   await addDoc(collection(db, "news"), {
     ...news
+  });
+};
+
+export const subscribeToNews = (callback) => {
+  return onSnapshot(collection(db, "news"), (snapshot) => {
+    const data = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+
+    callback(data);
   });
 };
